949a92c9500fb95b4146aa57c9321229e8f3103f
kotarott <kg221kg@gmail.com>
2021-08-22 11:55:01 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Python_exercise007
-翻訳アプリ（Google翻訳API）
+翻訳アプリ（Google翻訳API）  
+  
+
+## インストール(exercise/main.py)
+pip install googletrans  
+↓上手くいかない時  
+pip uninstall googletrans  
+pip install googletrans==4.0.0-rc1  
+  
+
+
+
---
+++ view.py
@@ -0,0 +1,12 @@
+import eel
+import desktop
+
+app_name = "web"
+end_point = "index.html"
+size = (600,700)
+
+
+# @ eel.expose
+
+desktop.start(app_name,end_point,size)
+#desktop.start(size=size,appName=app_name,endPoint=end_point)
